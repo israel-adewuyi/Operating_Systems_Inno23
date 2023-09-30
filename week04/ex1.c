581bbb8d35d5e7445d232d450279dbb7a5dd2378
israel <israel@Ubuntu.myguest.virtualbox.org>
2023-09-30 20:35:43 +0100

    First Task

---
+++ week04/ex1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+int main(){
+    pid_t first_child = fork();
+    pid_t second_child;
+
+    clock_t start_time = clock(), end_time;
+
+    if(first_child == 0){
+        end_time = clock();
+        printf("The ID of the first process is : %d\nThe parent of the first process is :%d\nThe execution time of the first process is %f\n\n", getpid(), getppid(), (double)end_time - start_time * 1000.0 / CLOCKS_PER_SEC);
+        exit(0);
+    }
+
+    printf("My ID %d, My parent ID %d\n", getpid(), getppid());
+
+    
+    second_child = fork();
+    start_time = clock();
+
+
+    if(second_child == 0){
+        end_time = clock();
+        printf("The ID of the first process is : %d\nThe parent of the first process is :%d\nThe execution time of the first process is %f\n\n", getpid(), getppid(), (double)end_time - start_time * 1000.0 / CLOCKS_PER_SEC);
+        exit(0);
+    }
+
+
+
+
+    return 0;
+}
